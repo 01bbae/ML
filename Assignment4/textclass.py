40c8a5e8065ff76e93ed4e0a9413398d339bd140
root <root@fowler-lpw-dgx-0.chapman.edu>
2023-04-20 22:16:56 +0000

    started textclass.py

---
+++ Assignment4/textclass.py
@@ -0,0 +1,87 @@
+import csv
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, LSTM, Dropout, Activation, Embedding, Bidirectional
+
+
+# !wget --no-check-certificate \
+#     https://storage.googleapis.com/dataset-uploader/bbc/bbc-text.csv \
+#     -O /tmp/bbc-text.csv
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+STOPWORDS = set(stopwords.words('english'))
+
+vocab_size = 5000 # make the top list of words (common words)
+embedding_dim = 64
+max_length = 200
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = '<OOV>' # OOV = Out of Vocabulary
+training_portion = .8
+
+articles = []
+labels = []
+
+with open("./tmp/bbc-text.csv", 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    next(reader)
+    for row in reader:
+        labels.append(row[0])
+        article = row[1]
+        for word in STOPWORDS:
+            token = ' ' + word + ' '
+            article = article.replace(token, ' ')
+            article = article.replace(' ', ' ')
+        articles.append(article)
+
+train_size = int(len(articles) * training_portion)
+
+train_articles = articles[0: train_size]
+train_labels = labels[0: train_size]
+
+validation_articles = articles[train_size:]
+validation_labels = labels[train_size:]
+
+print("train_size", train_size)
+
+print(f"train_articles {len(train_articles)}")
+
+print("train_labels", len(train_labels))
+
+print("validation_articles", len(validation_articles))
+
+print("validation_labels", len(validation_labels))
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(train_articles)
+word_index = tokenizer.word_index
+
+train_sequences = tokenizer.texts_to_sequences(train_articles)
+train_padded = pad_sequences(train_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+validation_sequences = tokenizer.texts_to_sequences(validation_articles)
+validation_padded = pad_sequences(validation_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+label_tokenizer = Tokenizer()
+label_tokenizer.fit_on_texts(labels)
+
+training_label_seq = np.array(label_tokenizer.texts_to_sequences(train_labels))
+validation_label_seq = np.array(label_tokenizer.texts_to_sequences(validation_labels))
+
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim))
+model.add(Dropout(0.5))
+model.add(Bidirectional(LSTM(embedding_dim)))
+model.add(Dense(6, activation='softmax'))
+model.summary()
+# opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+# model.compile(
+#     loss='sparse_categorical_crossentropy',
+#     optimizer=opt,
+#     metrics=['accuracy'],
+# )
